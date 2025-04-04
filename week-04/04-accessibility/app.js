592d64a54db69b7838cf86bd64fb03bc8c581987
frank-ventures <6458936+frank-ventures@users.noreply.github.com>
2025-04-04 15:08:16 +0100

    accessibility

---
+++ week-04/04-accessibility/app.js
@@ -0,0 +1,2 @@
+// When we're ready and armed with skills and knowledge, we'll use JS to randomly select a placeholder for an input
+const placeholderTexts = ["silly cats", "awesome parkour", "fine art painting"];
