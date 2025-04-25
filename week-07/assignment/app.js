758e3c2c750c7fc1c8f523ddd3ba14c6f1bc9125
frank-ventures <6458936+frank-ventures@users.noreply.github.com>
2025-04-25 15:20:13 +0100

    assignment skeleton

---
+++ week-07/assignment/app.js
@@ -0,0 +1,26 @@
+// --- --- ---
+// With my CSS, I want to make sure my app is designed from a mobile first perspective, and also looks good on desktop.
+
+// Decide on two Design Principles to use:
+// 1:
+// 2:
+// and ALSO, write about them in my reflection
+
+// Check all inputs have a label.
+
+// Pick at least one accessibility consideration
+// 1:
+// and ALSO mention what I did in my reflection
+
+// --- --- ---
+// I need to make sure that I have created a form in my HTML
+// I need to gert my javascript to select that form
+// I need to add an event listener to my form, so that when it is submitted, it gets the user data and calls a fetch function
+// NOTE TO SELF: remember to look for those two fancy lines of code which create a formData object for me.
+
+// I need to make a function which makes a fetch request to the API I chose.
+// Hmmmmmmmm, wasn't there something about async/await?.....
+// We spent a lot of time on Query Strings. Maybe that's useful for my fetch() request
+
+// Once I have made my fetch request, I should render those results on the page.
+// Didn't Frankie use a loop for that? Something to test out.
