acf2acf9cc86eec8d30d1964767cf6939408f2d6
frank-ventures <6458936+frank-ventures@users.noreply.github.com>
2025-05-08 15:01:40 +0100

    demo with query strings

--- week-09/02-connecting-database-to-server/server.js
+++ week-09/02-connecting-database-to-server/server.js
@@ -21,14 +21,29 @@ app.get("/", (request, response) => {
   response.json("You have reached the root. Don't trip!");
 });
 
-app.get("/testroute", (request, response) => {
-  response.json("You have reached the test route!");
-});
-
 app.get("/books", async function (req, res) {
   const books = await db.query(`SELECT * FROM books_002`);
   console.log(books.rows);
   res.json(books.rows);
+});
+
+// Query strings!
+// Here we have a route dedicated to searching our books by year
+app.get("/booksyearquery", async function (request, response) {
+  // http://localhost:8080/booksyearquery?year=1964  <- this is the URL we went to in our browser
+
+  console.log("current query: ", request.query); // <- To see the what is in the request.query object we can console.log like this
+
+  console.log(request.query.year);
+  const yearQuery = request.query.year; // <- This is how we assign a specific part of the query to a variable
+
+  const queriedBooks = await db.query(
+    `SELECT * FROM books_002 WHERE publish_date = ${yearQuery}`
+  ); // <- Here is how we can use that variable as part of our database query
+
+  console.log(queriedBooks.rows);
+
+  response.json(queriedBooks.rows); // <- Finally, we send that back to the client
 });
 
 app.listen(PORT, function () {
