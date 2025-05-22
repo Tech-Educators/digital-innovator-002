2391f19448b0d6de019d017e624b95f1484fa837
frank-ventures <6458936+frank-ventures@users.noreply.github.com>
2025-05-22 10:08:38 +0100

    added note on vs code breakpoints

--- week-11/01-debugger/app.js
+++ week-11/01-debugger/app.js
@@ -1,4 +1,4 @@
-debugger; // this will stop the code running here, and open the debugger in the browsers dev tools
+// debugger; // this will stop the code running here, and open the debugger in the browsers dev tools
 console.log("I am in the console!");
 
 const frankiesNumber = 70;
@@ -15,7 +15,10 @@ countToTen();
 function add(a, b) {
   let sum = a + b;
   console.log(sum);
-  debugger;
+  //debugger;
 }
 
 add(5, 10);
+
+// We can also add 'breakpoints' in the 'gutter' on VS Code. This is useful if you want to debug your javascript inside of VS code itself.
+// https://code.visualstudio.com/docs/debugtest/debugging
